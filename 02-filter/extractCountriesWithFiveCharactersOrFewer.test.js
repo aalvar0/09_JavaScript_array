249929ab92ed7a5be109a58b73b0583e7bb4f3a1
alvaro <alvaro.herranz@mataro.epiaedu.cat>
2024-01-16 20:06:32 +0100

    exercises deleted

--- 02-filter/extractCountriesWithFiveCharactersOrFewer.test.js
+++ 02-filter/extractCountriesWithFiveCharactersOrFewer.test.js
@@ -1,10 +1,3 @@
-import extractCountriesWithFiveCharactersOrFewer from "./extractCountriesWithFiveCharactersOrFewer.js";
-
-const COUNTRIES = ["United Kingdom", "Italy", "France", "Portugal", "Greece"];
-
 describe("", () => {
-  test("extractCountriesWithFiveCharactersOrFewer returns correct array", () => {
-    const result = extractCountriesWithFiveCharactersOrFewer(COUNTRIES);
-    expect(result).toEqual(["Italy"]);
-  });
+  test("", () => {});
 });
